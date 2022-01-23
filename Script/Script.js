b78bb4853e65044dc598d785b1b4ebecb0357b57
ravi <ravisharma45935@gmail.com>
2022-01-23 21:53:32 +0530

    updated

--- Script/Script.js
+++ Script/Script.js
@@ -1,72 +1,82 @@
-console.log('I am running here')
-function validation(){
-    var user = document.getElementById('username').value;
+function clearErrors() {
+    var errors = document.getElementsByTagName('span');
+    for (let item of errors) {
+        item.innerHTML = '';
+    }
+}
+
+function validation() {
+    clearErrors();
+    // Colleting values from input
+    var username = document.getElementById('username').value;
     var email = document.getElementById('email').value;
-    var number = document.getElementById('phone-number').value;
+    var phone = document.getElementById('phone-number').value;
     var password = document.getElementById('password').value;
-    var cpassword = document.getElementById('confirm-password').value;
+    var cpassword = document.getElementById('cpassword').value;
 
-
-
-
-
-    if(user == ""){
-        document.getElementById('userAlert').innerHTML = '** username Required';
-        return false;
-    }
-    if((user.length <=2) || (user.length >12)){
-        document.getElementById('userAlert').innerHTML = '** username must be between 2 to 12';
-        return false;
-    }
-    if(!isNaN(user)){
-        document.getElementById('userAlert').innerHTML = '** username must be character only';
+    // username validation
+    if (username == '') {
+        document.getElementById('userAlert').innerHTML = " ** Username required";
         return false;
     }
 
-    if(email == ""){
-        document.getElementById('emailAlert').innerHTML = '** email Required';
+    else if (username.length <= 2 || username.length >= 20) {
+        document.getElementById('userAlert').innerHTML = " ** Username must be 2 to 20 character only";
         return false;
     }
-    if(email.indexOf('@') <= 0){
-        document.getElementById('emailAlert').innerHTML = '** Invalid position of @';
-        return false;
-    }
-    if((email.charAt(email.length-4)!='.') && (email.charAt(email.length-4)!='.')){
-        document.getElementById('emailAlert').innerHTML = '** . invalid position';
+    else if (!isNaN(username)) {
+        document.getElementById('userAlert').innerHTML = " ** Username must be characters only";
         return false;
     }
 
 
-    if(number == ""){
-        document.getElementById('numberAlert').innerHTML = '** number Required';
+    // user email validation
+    if (email == '') {
+        document.getElementById('emailAlert').innerHTML = " ** email required";
         return false;
     }
-    if(isNaN(number)){
-        document.getElementById('numberAlert').innerHTML = '** number must be digit only';
+    else if (email.indexOf('@') <= 0) {
+        document.getElementById('emailAlert').innerHTML = " ** Invalid email";
         return false;
     }
-
-    if(number.length !=10){
-        document.getElementById('numberAlert').innerHTML = '** mobile number must be 10 digit only';
-        return false;
-    }
-
-
-    if(password == ""){
-        document.getElementById('passwordAlert').innerHTML = '** password Required';
-        return false;
-    }
-    if((password.length <=5) || (password.length >10)){
-        document.getElementById('passwordAlert').innerHTML = '** password must be contain 5 to 10 character';
+    else if ((email.charAt(email.length - 4) != '.') && (email.charAt(email.length - 3) != '.')) {
+        document.getElementById('emailAlert').innerHTML = '** invalid email';
         return false;
     }
 
-    if(cpassword == ""){
-        document.getElementById('cpasswordAlert').innerHTML = '** password confirmation Required';
+
+    // user phone number validation
+    if (phone == '') {
+        document.getElementById('numberAlert').innerHTML = " ** Phone Nomber required";
         return false;
     }
-   if(password!=cpassword){
-    document.getElementById('cpasswordAlert').innerHTML = `** password didn't matched`;
-    return false;
-   }
+    if (isNaN(phone)) {
+        document.getElementById('numberAlert').innerHTML = " ** Phone Nomber must be digits only";
+        return false;
+    }
+    if (phone.length != 10) {
+        document.getElementById('numberAlert').innerHTML = " ** Phone Nomber must be 10 digits only";
+        return false;
+    }
+
+
+    // user password validation
+    if (password == '') {
+        document.getElementById('passworAlert').innerHTML = " ** Password required";
+        return false;
+    }
+    else if (password.length <= 3 || password.length >= 15) {
+        document.getElementById('passworAlert').innerHTML = " ** Password must be 3 to 15 characters only";
+        return false;
+    }
+
+    // user confirm password validation and checking
+    if (cpassword == '') {
+        document.getElementById('cpasswordAlert').innerHTML = " ** Password Confirmation required";
+        return false;
+    }
+    if (cpassword != password) {
+        document.getElementById('cpasswordAlert').innerHTML = " ** Password didn't matched";
+        return false;
+    }
 }
